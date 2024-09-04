28f2207c6d6cf166366d85ad26e8902ad7db43ac
felippemozer <felippemozer22@gmail.com>
2024-09-04 13:56:44 -0300

    feat: final mobile version

--- projects/notes/sls-notes-mobile/src/app/app.module.ts
+++ projects/notes/sls-notes-mobile/src/app/app.module.ts
@@ -3,12 +3,19 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { GooglePlus } from '@ionic-native/google-plus';
+
+import { HttpClientModule } from '@angular/common/http';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { NotesPage } from '../pages/notes/notes';
 import { NotePage } from '../pages/note/note';
 import { AutoSizeDirective } from './directives/auto-size/auto-size';
+import { NotesApiService } from './services/notes-api/notes-api.services';
+import { TitlePipe } from './pipes/extract-title.pipe';
+import { AuthService } from './services/auth/auth.service';
+
 
 @NgModule({
   declarations: [
@@ -16,10 +23,12 @@ import { AutoSizeDirective } from './directives/auto-size/auto-size';
     HomePage,
     NotesPage,
     NotePage,
-    AutoSizeDirective
+    AutoSizeDirective,
+    TitlePipe
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -32,7 +41,10 @@ import { AutoSizeDirective } from './directives/auto-size/auto-size';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    NotesApiService,
+    AuthService,
+    GooglePlus
   ]
 })
 export class AppModule {}
